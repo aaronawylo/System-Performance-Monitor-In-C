fc9444b19869e3b06899e7984a0ebeb044dc8707
Aaron Lo <alo76@my.bcit.ca>
2023-11-14 10:27:26 -0800

    Delete CMakeLists.txt

--- Memory/Memory.c
+++ Memory/Memory.c
@@ -1,3 +1,28 @@
-//
-// Created by Aaron on 11/14/2023.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <unistd.h>
+#include <psapi.h>
+
+void print_memory_usage() {
+    PROCESS_MEMORY_COUNTERS pmc;
+
+    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
+        // Print memory usage information
+        printf("Memory usage: %u KB\n", (unsigned int)(pmc.WorkingSetSize / 1024));
+    } else {
+        perror("Error getting process memory info");
+    }
+}
+
+int memoryMain() {
+    int i = 0;
+    while (i < 5) {
+        // Print memory usage every 5 seconds
+        print_memory_usage();
+        Sleep(5000);  // Sleep for 5 seconds (note: Sleep uses milliseconds)
+        i += 1;
+    }
+
+    return 0;
+}
