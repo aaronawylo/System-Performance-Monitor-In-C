0b04cf5ed32e2789dbe0c7a8fa82f39221676e03
Nikita Olechko <nikita.olechko@gmail.com>
2023-11-14 10:17:44 -0800

    Create functional CPU monitoring system

--- CPU/CPUUtilities.c
+++ CPU/CPUUtilities.c
@@ -1,46 +1,37 @@
+#include <windows.h>
+#include <pdh.h>
+#include <pdhmsg.h>
 #include <stdio.h>
-#include <math.h>
 
-int removeLeftMostValue(int original, int originalLength){
-    int firstDigit = original / pow(10, originalLength - 1);
-    int multiplier = pow(10, originalLength-1);
-    return original - firstDigit * multiplier ;
+PDH_HQUERY cpuQuery;
+PDH_HCOUNTER cpuTotal;
+
+void setupPdhQuery() {
+    PdhOpenQuery(NULL, 0, &cpuQuery);
+    // This counter path works on English Windows systems. It may be different in other languages.
+    PdhAddEnglishCounter(cpuQuery, "\\Processor(_Total)\\% Processor Time", 0, &cpuTotal);
+    PdhCollectQueryData(cpuQuery);
 }
 
-int getLeftMostValue(int original, int originalLength){
-    int firstDigit = original / pow(10, originalLength - 1);
-    return firstDigit;
+double getCurrentCpuUsage() {
+    PDH_FMT_COUNTERVALUE counterVal;
+
+    PdhCollectQueryData(cpuQuery);
+    PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
+    return counterVal.doubleValue;
 }
 
-int getOddPositionedDigits(int original){
-    int oddPositionedDigits = 0;
-    int originalLength = floor(log10(abs(original))) + 1;
-    int truncatedNumber = removeLeftMostValue(original, originalLength);
-    if (originalLength <= 0){
-        return 0;
-    }
-    if (originalLength %2 != 0){
-        int multiplier;
-        if (originalLength > 1){
-            multiplier = pow(10, originalLength/2);
-        }
-        else {
-            multiplier = 1;
-        }
-        int increment = getLeftMostValue(original, originalLength)*multiplier;
-        oddPositionedDigits += increment;
-    }
-    return oddPositionedDigits += getOddPositionedDigits(truncatedNumber);
-}
 
 int main() {
-    int original = 89201;
-    printf("%d\n", getOddPositionedDigits(original));
-    original = 123;
-    printf("%d\n", getOddPositionedDigits(original));
-    original = 20;
-    printf("%d\n", getOddPositionedDigits(original));
-    original = 87654321;
-    printf("%d\n", getOddPositionedDigits(original));
+    setupPdhQuery();
+
+    while (1) {
+        printf("Current CPU Usage: %.2f%%\n", getCurrentCpuUsage());
+        Sleep(1000); // Sample every second
+    }
+
+    // Close the query
+    PdhCloseQuery(cpuQuery);
+
     return 0;
 }
