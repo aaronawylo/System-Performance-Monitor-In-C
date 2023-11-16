cf94269b8096eeaa852b6fac35fc1c0a4a139c92
Mika <mkmanaligod@gmail.com>
2023-11-16 12:12:15 -0800

    update Uptime.c

--- Uptime/Uptime.c
+++ Uptime/Uptime.c
@@ -2,14 +2,14 @@
 #include <windows.h>
 
 void displaySystemUptime() {
-    unsigned int uptime = GetTickCount64(); //can be unsigned since sys uptime is always non-negative
+    unsigned int uptime = GetTickCount64();
     unsigned int hours = uptime / 3600000;
     unsigned int minutes = (uptime % 3600000) / 60000;
 
     printf("The system has been up for %u hours and %u minutes.\n", hours, minutes);
 }
 
- int main() {
-     displaySystemUptime();
-     return 0;
- }
+// int main() {
+//     displaySystemUptime();
+//     return 0;
+// }
