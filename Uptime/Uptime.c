3b737e6eb332c7b3c6e177c08c9a0ceb98e74bb2
Mika <mkmanaligod@gmail.com>
2023-11-16 12:02:36 -0800

    modified Uptime.c

--- Uptime/Uptime.c
+++ Uptime/Uptime.c
@@ -3,8 +3,8 @@
 
 void displaySystemUptime() {
     unsigned int uptime = GetTickCount64(); //can be unsigned since sys uptime is always non-negative
-    int hours = uptime / 3600000;
-    int minutes = (uptime % 3600000) / 60000;
+    unsigned int hours = uptime / 3600000;
+    unsigned int minutes = (uptime % 3600000) / 60000;
 
     printf("The system has been up for %u hours and %u minutes.\n", hours, minutes);
 }
