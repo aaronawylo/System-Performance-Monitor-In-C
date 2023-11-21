7eea1e738db87829c9cc8d30fbeefe10f008a9ff
Mika <mkmanaligod@gmail.com>
2023-11-21 10:07:30 -0800

    fix borders

--- raylib/raygui.c
+++ raylib/raygui.c
@@ -10,6 +10,7 @@
 #include "style_cyber.h"
 #include "../User/CurrentUser.h"
 
+
 void DrawGraph(int *values, int count, Rectangle bounds, Color color) {
     if (count > 1) {
         int maxValue = 0;
@@ -25,10 +26,18 @@ void DrawGraph(int *values, int count, Rectangle bounds, Color color) {
     }
 }
 
+void DrawMainContentGrid(Rectangle bounds, int spacing, int subdivs) {
+    Vector2 mouseCell = {0, 0};
+    GuiGrid(bounds, "", spacing, subdivs, &mouseCell);
+    for (int i = 0; i < subdivs; i++) {
+        Rectangle cell = {bounds.x + i * (bounds.width / subdivs), bounds.y, bounds.width / subdivs, bounds.height};
+    }
+}
+
 int main() {
     //application window size
-    int screenWidth = 1280;
-    int screenHeight = 720;
+    int screenWidth = 1400;
+    int screenHeight = 800;
     //initalize window
     InitWindow(screenWidth, screenHeight, "");
     //load the cyber GUI style
@@ -45,9 +54,10 @@ int main() {
     double cpuUsage = 0.0;
     int memoryUsage = 0;
     //Inner window
-    Rectangle panelRec = { 5, 5, (float)screenWidth - 10, (float)screenHeight -10 };
-
-    // Graph data
+    Rectangle panelRec = { 2, 2, (float)screenWidth-4, (float)screenHeight-4 };
+    //close button
+    int closeButtonSize = 20;
+    // graph data
     int cpuHistory[GRAPH_HISTORY_LENGTH] = { 0 };
     int memoryHistory[GRAPH_HISTORY_LENGTH] = { 0 };
     int historyIndex = 0;
@@ -75,9 +85,15 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         //internal window initalization
-        GuiPanel(panelRec, "Super Awesome System Activity Monitor");
+        GuiWindowBox(panelRec, "Super Awesome Activiy Monitor");
         //title
         DrawText("SYSTEM MONITOR", 30, 70, 30, GetColor(0x3299b4ff));
+        //GUI grid -
+        Rectangle mainContentBounds = { 5, 5, screenWidth - 10, screenHeight- 10  }; // Adjust as needed
+        int mainContentSpacing = 25;
+        int mainContentSubdivs = 6;
+//        DrawMainContentGrid(mainContentBounds, mainContentSpacing, mainContentSubdivs);
+
         //rectangles for graphs (not visible)
         Rectangle cpuGraphRec = { 50, 150, 300, 100 };
         DrawGraph(cpuHistory, GRAPH_HISTORY_LENGTH, cpuGraphRec, GetColor(0x81c0d0ff));
@@ -95,12 +111,12 @@ int main() {
         int boxHeight = 35;
         int boxY = screenHeight - boxHeight - 5;
         //rectangle dimensions
-        Rectangle box1 = { 5, boxY, 200, boxHeight };
-        Rectangle box2 = { 5 + 200, boxY, 150, boxHeight };
-        Rectangle box3 = { 5 + 200+150, boxY, 275, boxHeight };
-        Rectangle box4 = { 5 + 200+150+275, boxY, 200, boxHeight };
-        Rectangle box5 = { 5 + 200+150+275+200, boxY, 200, boxHeight };
-        Rectangle box6 = { 5 + 200+150+275+200+200, boxY, screenWidth-165-150-300-200-200-20, boxHeight };
+        Rectangle box1 = { 5, boxY, 120, boxHeight };
+        Rectangle box2 = { 5 + 120, boxY, 150, boxHeight };
+        Rectangle box3 = { 5 + 120+150, boxY, 210, boxHeight };
+        Rectangle box4 = { 5 + 120+150+210, boxY, 280, boxHeight };
+        Rectangle box5 = { 5 + 120+150+210+280, boxY, 250, boxHeight };
+        Rectangle box6 = { 5 + 120+150+210+280+250, boxY, screenWidth-165-150-300-200-200-6, boxHeight };
         //draw the rectangles
         DrawRectangleRec(box1, GetColor(0x024658ff));
         DrawRectangleRec(box2, GetColor(0x024658ff));
@@ -118,12 +134,12 @@ int main() {
         DrawRectangleLinesEx(box5, borderWidth, borderColor);
         DrawRectangleLinesEx(box6, borderWidth, borderColor);
         //text in rectangles
-        DrawText("CURRENT USER: ", box1.x + 20, box1.y + 8, 18, GetColor(0x51bfd3ff));
-        DrawText(userName, box2.x + 20, box2.y + 8, 18, GetColor(0x51bfd3ff));
-        DrawText("CURRENT DATE & TIME: ", box3.x + 20, box3.y + 8, 18, GetColor(0x51bfd3ff));
-        DrawText(dateTimeStr, box4.x + 22, box4.y + 8, 18, GetColor(0x51bfd3ff));
-        DrawText("SYSTEM UPTIME: ", box5.x + 20, box5.y + 8, 18, GetColor(0x51bfd3ff));
-        DrawText(uptimeStr, box6.x + 22, box6.y + 8, 18, GetColor(0x51bfd3ff));
+        DrawText("USER: ", box1.x + 25, box1.y + 8, 20, GetColor(0x51bfd3ff));
+        DrawText(userName, box2.x + 25, box2.y + 8, 20, GetColor(0x51bfd3ff));
+        DrawText("DATE & TIME: ", box3.x + 25, box3.y + 8, 20, GetColor(0x51bfd3ff));
+        DrawText(dateTimeStr, box4.x + 25, box4.y + 8, 20, GetColor(0x51bfd3ff));
+        DrawText("SYSTEM UPTIME: ", box5.x + 25, box5.y + 8, 20, GetColor(0x51bfd3ff));
+        DrawText(uptimeStr, box6.x + 25, box6.y + 8, 20, GetColor(0x51bfd3ff));
 
         EndDrawing();
     }
