5f7be08027fe6699078d5b243c8b0cd26e5680a0
MADDELIN MADDELIN <maddelin.tania@gmail.com>
2023-11-22 18:22:21 -0800

    Modify header file params to match module functions

--- Disk_IO/DiskIO_monitoring.h
+++ Disk_IO/DiskIO_monitoring.h
@@ -9,8 +9,8 @@
 double getDiskTotal(ULARGE_INTEGER total);
 double getDiskFreeSpace(ULARGE_INTEGER free);
 double getDiskTotalFree();
-double getDiskRead();
-double getDiskWrite();
+double getDiskRead(const char *diskName, DISK_PERFORMANCE diskPerformance);
+double getDiskWrite(const char *diskName, DISK_PERFORMANCE diskPerformance);
 double calculateDiskUsagePercentage();
 
 #endif //DISKIO_MONITORING_H
